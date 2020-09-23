2ebc233e010a1b3abb62ee75f3b499f8c5296f56
Michelhdez <michel01406@gmail.com>
2020-09-23 16:47:06 -0500

    Primer commit

---
+++ js/assets.js
@@ -0,0 +1,18 @@
+// (function (d) {
+//     let tabs = Array.prototype.slice.apply(d.querySelectorAll('.tabs'));
+//     let panels = Array.prototype.slice.apply(d.querySelectorAll('.panels_item'));
+//     d.getElementById('tabs').addEventListener('click', e => {
+//         if (e.target.classList.contains('tabs')) {
+//             let i = tabs.IndexOf(e.target);
+//             tabs.map(tab => tab.classList.remove('active'));
+//             tabs[i].classList.add('active');
+
+//             panels.map(panel => panel.classList.remove('active'));
+//             panels[i].classList.add('active');
+//         }
+//     });
+// })(document);
+
+// tabs
+
+
